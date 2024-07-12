a7262deee2a6873c0d38283fc7b1fabff2a622c6
Sabir Ali <alisabir167167@gmail.com>
2024-07-12 17:35:10 +0530

    Add server.js and Procfile for Heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // Import Font Awesome CSS
 import { config } from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; // Disable the auto add CSS
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+// Disable the auto add CSS for Font Awesome
+config.autoAddCss = false;
 
+// Create the root for concurrent mode rendering (React 17+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
@@ -17,7 +18,5 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app
 reportWebVitals();
