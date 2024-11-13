8eadea708b65dccea5383516c6c36c515fa95b20
Sabir Ali <alisabir167167@gmail.com>
2024-11-13 07:54:04 +0530

    LeaderTeam css changed

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -4,14 +4,12 @@ import AboutUsDescription from './aboutUsDescription';
 import OurStrengthOurPresence from './OurStrengthOurPresence';
 import LeaderTeam from './LeaderTeam';
 import PolicyIntegration from './PolicyIntegration';
-import Typewriter from 'typewriter-effect'; // Import Typewriter from 'typewriter-effect'
 
 class AboutUsContent extends Component {
   render() {
     return (
       <div>
         <div className='aboutus'>
-          {/* Apply Typewriter effect to this h1 element */}
           <h1>About <span className='quality'>US</span></h1>
          
         </div>
