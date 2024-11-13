62029149ea65e333e7155e678cc0bc6cc22ad8c5
Sabir Ali <alisabir167167@gmail.com>
2024-11-13 08:30:58 +0530

    Not Found style changed

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import './notFound.css'; 
 
 class NotFound extends Component {
-   state = {  } 
-   render() { 
-      return (
-         <div className='main-content'>
-            <h1>Page not found route</h1>
-         </div>
-
-      );
-   }
+  state = {}; 
+  render() { 
+    return (
+      <div className='Not-Found'>
+        <h1 >Page not found route</h1>
+      </div>
+    );
+  }
 }
- 
+
 export default NotFound;
